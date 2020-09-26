9b419b85a37527ee96796dec6381a8b4bebe1ab5
Jason Sievert <jsievert@gmail.com>
2020-09-26 10:18:34 -0500

    Testing 0.13 update

--- sievert_fun.tf
+++ sievert_fun.tf
@@ -1,7 +1,7 @@
-provider "cloudflare" {
+provider "cloudflare/cloudflare" {
   version = "~> 2.0"
   email   = var.cloudflare_email
-  api_key = "${var.cloudflare_api_key}"
+  api_key = var.cloudflare_api_key
 }
 
 
