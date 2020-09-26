598be308b16d83e94e95df232794e93e234568fb
Jason Sievert <jsievert@gmail.com>
2020-09-26 10:13:23 -0500

    Adding provider

--- sievert_fun.tf
+++ sievert_fun.tf
@@ -1,3 +1,10 @@
+provider "cloudflare" {
+  version = "~> 2.0"
+  email   = "${var.cloudflare_email}"
+  api_key = "${var.cloudflare_api_key}"
+}
+
+
 variable "domain" {
   default = "sievert.fun"
 }
