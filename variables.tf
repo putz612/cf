8d42a4c225cd57ae6e1ba07dea871bc850c5c733
Jason Sievert <jsievert@gmail.com>
2021-10-16 09:02:21 -0500

    Moving things over to k3s

--- variables.tf
+++ variables.tf
@@ -13,24 +13,6 @@ variable "aRecords" {
   description = "All the ipv4 A recored"
   type        = list(tuple([string, string, bool]))
   default = [
-    ["chronograf", "192.168.100.170", false],
-    ["chronograf", "192.168.100.171", false],
-    ["chronograf", "192.168.100.172", false],
-    ["esphome", "192.168.100.170", false],
-    ["esphome", "192.168.100.171", false],
-    ["esphome", "192.168.100.172", false],
-    ["hass", "192.168.5.100", false],
-    ["hass", "192.168.5.101", false],
-    ["hass", "192.168.5.102", false],
-    ["influxdb", "192.168.100.170", false],
-    ["influxdb", "192.168.100.171", false],
-    ["influxdb", "192.168.100.172", false],
-    ["grafana", "192.168.100.170", false],
-    ["grafana", "192.168.100.171", false],
-    ["grafana", "192.168.100.172", false],
-    ["netbox", "192.168.100.170", false],
-    ["netbox", "192.168.100.171", false],
-    ["netbox", "192.168.100.172", false],
     ["grocery", "192.168.100.170", false],
     ["grocery", "192.168.100.171", false],
     ["grocery", "192.168.100.172", false],
@@ -53,11 +35,7 @@ variable "aRecords" {
     ["sonarr", "192.168.5.101", false],
     ["sonarr", "192.168.5.102", false],
     ["ebooks", "192.168.5.100", false],
-    ["ebooks", "192.168.5.101", false],
-    ["ebooks", "192.168.5.102", false],
-    ["esphome", "192.168.5.100", false],
-    ["esphome", "192.168.5.101", false],
-    ["esphome", "192.168.5.102", false]
+    ["ebooks", "192.168.5.101", false]
   ]
 }
 
